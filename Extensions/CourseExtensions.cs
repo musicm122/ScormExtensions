cd6dda7897fc86749220efe32bf2c8304969f713
Terrance Smith <musicm122@gmail.com>
2016-11-21 09:51:11 -0500

    Added Course and Registration Extension methods.

---
+++ Extensions/CourseExtensions.cs
@@ -0,0 +1,39 @@
+﻿using RusticiSoftware.HostedEngine.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerFerret.ScormHelper.Extensions
+{
+
+    public static class CourseExtensions
+    {
+        public static string ToFormattedString(this CourseData data)
+        {
+            return
+$@"Title: {data.Title}
+CourseId: {data.CourseId}
+Number of Registrations:    {data.NumberOfRegistrations}
+Number of Versions:    {data.NumberOfVersions}";
+
+        }
+
+
+        public static string ToCSVString(this CourseData data)
+        {
+            return $@"{data.Title}, {data.CourseId}, {data.NumberOfRegistrations}, {data.NumberOfVersions}";
+        }
+
+        public static string ToCSVString(this List<CourseData> dataList)
+        {
+            var rowSeperator = "\r\n";
+            var result = dataList
+                    .Select<CourseData, string>(c => c.ToCSVString())
+                    .Aggregate((x, y) => x + rowSeperator + y);
+            return result;
+        }
+
+    }
+}
---
+++ Extensions/RegistrationExtensions.cs
@@ -0,0 +1,61 @@
+﻿using RusticiSoftware.HostedEngine.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerFerret.ScormHelper.Extensions
+{
+    public static class RegistrationExtensions
+    {
+
+
+        public static string ToFormattedString(this RegistrationData data)
+        {
+            return
+$@"CompletedDate: {data.CompletedDate}
+CourseId: {data.CourseId}
+CourseTitle: {data.CourseTitle}
+CreateDate: {data.CreateDate}
+Email: {data.Email}
+FirstAccessDate: {data.FirstAccessDate}
+Instances: {data.Instances.ToCSVString()}
+LastAccessDate: {data.LastAccessDate}
+LastCourseVersionLaunched: {data.LastCourseVersionLaunched}
+LearnerFirstName: {data.LearnerFirstName}
+LearnerId: {data.LearnerId}
+LearnerLastName: {data.LearnerLastName}
+RegistrationId: {data.RegistrationId}";
+        }
+
+        public static string ToCSVString(this RegistrationData data)
+        {
+            return $@"{ data.CompletedDate}, { data.CourseId}, { data.CourseTitle}, { data.CreateDate}, { data.Email}, { data.FirstAccessDate}, { data.Instances}, { data.LastAccessDate}, { data.LastCourseVersionLaunched}, { data.LearnerFirstName}, { data.LearnerId}, { data.LearnerLastName}, { data.RegistrationId}";
+        }
+
+
+        public static string ToCSVString(this List<RegistrationData> dataList)
+        {
+            var rowSeperator = "\r\n";
+            var result = dataList
+                    .Select<RegistrationData, string>(r => r.ToCSVString())
+                    .Aggregate((x, y) => x + rowSeperator + y);
+            return result;
+        }
+
+        public static string ToCSVString(this List<InstanceData> InstanceData)
+        {
+            var rowSeperator = "\r\n";
+
+            if (InstanceData.Count == 0)
+            {
+                return String.Empty;
+            }
+            var result = InstanceData
+                    .Select<InstanceData, string>(r => r.CourseVersion + "," + r.InstanceId + "," + r.UpdateDate)
+                    .Aggregate((x, y) => x + rowSeperator + y);
+            return result;
+        }
+    }
+}
